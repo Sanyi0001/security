fbfaee9402d123d67e48d25cb19e5b63428bfb85
Sanyi0001 <70522920+Sanyi0001@users.noreply.github.com>
2021-05-10 14:58:48 +0200

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,8 @@
         <link href="/css/app.css" rel="stylesheet">
 
         <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <script src="'/js/app.js'" defer></script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
