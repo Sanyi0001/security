c4795c8a63fd90c3133252786ff608db4986a323
Sanyi0001 <70522920+Sanyi0001@users.noreply.github.com>
2021-05-18 21:44:14 +0200

    added error validation

--- resources/views/cars/edit.blade.php
+++ resources/views/cars/edit.blade.php
@@ -3,67 +3,68 @@
 @section('body')
     <div id="wrapper">
         <div id="page">
-            <h1 class="underline text-green-600 text-xl font-extrabold">Edit Car</h1>
-            <form method="POST" action="{{route('cars.show', $car)}}">
+            <br>
+            <div class="md:flex md:items-center mb-6">
+                <div class="md:w-1/3">
+                    <h1 class="block text-green-600 font-bold text-center mb-1 md:mb-0 pr-4">Edit Car</h1>
+                </div>
+            </div>
+            <form class="w-full max-w-sm" method="POST" action="{{route('cars.show', $car)}}">
                 @csrf
                 @method('PUT')
-
-                <div class="field">
-                    <label class="label" for="registration_number">Registration number</label>
-                    <div class="control">
-                        <input class="input {{$errors->has('registration_number') ? 'is-danger' : ''}}" type="text"
-                               name="registration_number" id="registration_number"
-                               value="{{$car->registration_number}}">
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                               for="registration_number">Registration number
+                        </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <input
+                            class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 {{$errors->has('registration_number') ? 'is-danger' : ''}}"
+                            id="registration_number" name="registration_number" type="text"
+                            value="{{$car->registration_number}}">
                         @error('registration_number')
-                        <p class="help is-danger">{{$errors->first('registration_number')}}</p>
+                        <p class="text-red-600">{{$errors->first('registration_number')}}</p>
                         @enderror
                     </div>
                 </div>
-
-                <div class="field">
-                    <label class="label" for="manufacturer">Manufacturer</label>
-                    <div class="control">
-                        <input class="input {{$errors->has('manufacturer') ? 'is-danger' : ''}}" type="text"
-                               name="manufacturer" id="manufacturer" value="{{$car->manufacturer}}">
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="manufacturer">
+                            Manufacturer
+                        </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <input
+                            class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 {{$errors->has('manufacturer') ? 'is-danger' : ''}}"
+                            id="manufacturer" name="manufacturer" type="text" value="{{$car->manufacturer}}">
                         @error('manufacturer')
-                        <p class="help is-danger">{{$errors->first('manufacturer')}}</p>
+                        <p class="text-red-600">{{$errors->first('manufacturer')}}</p>
                         @enderror
-
                     </div>
                 </div>
-                <br>
-                <div class="field">
-                    <label class="label" for="currently_available">Currently available?</label>
-                    <select class="input {{$errors->has('currently_available') ? 'is-danger' : ''}}" type="select"
-                            name="currently_available" id="currently_available" value="{{$car->currently_available}}">
-                        <option value="1">YES</option>
-                        <option value="0">NO</option>
-                    </select>
-                    @error('currently_available')
-                    <p class="help is-danger">{{$errors->first('currently_available')}}</p>
-                    @enderror
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3"></div>
+                    <label class="md:w-2/3 block text-gray-500 font-bold">
+                        <input type="hidden" id="currently_available" name="currently_available" value="0">
+                        <input class="mr-2 leading-tight" id="currently_available" name="currently_available"
+                               type="checkbox"
+                               @if($car->currently_available == 1)
+                               checked
+                               @endif
+                        <span class="text-sm">Currently available</span>
+                    </label>
                 </div>
-
-                <div class="field is-groupped">
-                    <div class="control">
-                        <button class="bg-green-600 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
-                                type="submit">Submit
+                <div class="md:flex md:items-center">
+                    <div class="md:w-1/3"></div>
+                    <div class="md:w-2/3">
+                        <button
+                            class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                            type="submit">Submit
                         </button>
                     </div>
                 </div>
-
             </form>
-            <br>
-            <div>
-                <form method="POST" action="{{route('cars.destroy', $car)}}">
-                    @csrf
-                    @method('DELETE')
-                    <button class="bg-green-600 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
-                            type="submit">Delete
-                    </button>
-                </form>
-            </div>
-
         </div>
     </div>
 @endsection
