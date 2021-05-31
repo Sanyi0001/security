89e997b830f1b4f36fce479bd64d316828847dc1
Sanyi0001 <70522920+Sanyi0001@users.noreply.github.com>
2021-05-31 16:55:20 +0200

    seeder updated

--- database/seeders/CarSeeder.php
+++ database/seeders/CarSeeder.php
@@ -14,13 +14,13 @@ class CarSeeder extends Seeder
     public function run()
     {
         \DB::table('cars')->insert([
-            'registration_number' => '12345678', 'manufacturer' => 'Ford', 'currently_available' =>1, 'contact_email' => "example1@gmail.com",'USD'=>1
+            'registration_number' => '12345678', 'manufacturer' => 'Ford', 'currently_available' =>1, 'contact_email' => "example1@gmail.com",'USD'=>5400
         ]);
         \DB::table('cars')->insert([
-            'registration_number' => '14368235', 'manufacturer' => 'Tesla', 'currently_available' =>0, 'contact_email' => "example1@gmail.com",'USD'=>2
+            'registration_number' => '14368235', 'manufacturer' => 'Tesla', 'currently_available' =>0, 'contact_email' => "example1@gmail.com",'USD'=>2970
         ]);
         \DB::table('cars')->insert([
-            'registration_number' => '14238845', 'manufacturer' => 'Audi', 'currently_available' =>1, 'contact_email' => "example2@gmail.com", 'USD'=>3
+            'registration_number' => '14238845', 'manufacturer' => 'Audi', 'currently_available' =>1, 'contact_email' => "example2@gmail.com", 'USD'=>12540
         ]);
     }
 }
