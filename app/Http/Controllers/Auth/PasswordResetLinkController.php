c69cc3e30d54eec629e05ae8f3961ddba2a785e1
Sanyi0001 <70522920+Sanyi0001@users.noreply.github.com>
2021-06-02 11:54:08 +0200

    added comments, modified forgotten password handling

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -21,7 +21,7 @@ class PasswordResetLinkController extends Controller
     /**
      * Handle an incoming password reset link request.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\RedirectResponse
      *
      * @throws \Illuminate\Validation\ValidationException
@@ -35,12 +35,12 @@ class PasswordResetLinkController extends Controller
         // We will send the password reset link to this user. Once we have attempted
         // to send the link, we will examine the response then see the message we
         // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::sendResetLink(
+        Password::sendResetLink(
             $request->only('email')
         );
 
-        return $status == Password::RESET_LINK_SENT
-                    ? back()->with('status', "If this email address exists in our system, we have emailed you a password reset link!")
-                    : back()->with('status', "If this email address exists in our system, we have emailed you a password reset link!");
+        // It doesn't matter whether the email address was found in the database, this text will be displayed to avoid
+        // revealing the corresponding users existence in the system.
+        return back()->with('status', "If this email address exists in our system, we have emailed you a password reset link!");
     }
 }
--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -17,11 +17,18 @@ class CarController extends Controller
     {
 
         $cars = Car::orderBy('registration_number', 'asc')->get();
+
+        // Connecting to the API and requesting the USD-EUR currency pair, the URL also contains the API key
         $response = Http::get('https://free.currconv.com/api/v7/convert?q=USD_EUR&compact=ultra&apiKey=9c7b50ffa8a208a7459f');
-        $ex = $response->json('USD_EUR');
-        foreach($cars as $car) {
-            $car->EUR = round($car->USD * $ex);
-    }
+
+        // Getting the USD_EUR exchange rate out of the json reply
+        $exchange_rate = $response->json('USD_EUR');
+
+        // Calculating and assigning a USD value for every car based on the EUR
+        foreach ($cars as $car) {
+            $car->EUR = round($car->USD * $exchange_rate);
+        }
+
         return view('cars.index', compact('cars'));
     }
 
@@ -38,10 +45,10 @@ class CarController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Car $car , Request $request)
+    public function store(Car $car, Request $request)
     {
         Car::create($this->validateCar($request));
         return redirect('/cars');
@@ -50,7 +57,7 @@ class CarController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Car  $car
+     * @param \App\Models\Car $car
      * @return \Illuminate\Http\Response
      */
     public function show(Car $car)
@@ -61,33 +68,33 @@ class CarController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Car  $car
+     * @param \App\Models\Car $car
      * @return \Illuminate\Http\Response
      */
     public function edit(Car $car)
     {
         $this->authorize('update-car');
-        return view('cars.edit',compact('car'));
+        return view('cars.edit', compact('car'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Car  $car
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Car $car
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Car $car)
     {
         $this->authorize('update-car');
         $car->update($this->validateCarUpdate($request));
-        return redirect(route('cars.show' , $car));
+        return redirect(route('cars.show', $car));
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Car  $car
+     * @param \App\Models\Car $car
      * @return \Illuminate\Http\Response
      */
     public function destroy(Car $car)
@@ -97,24 +104,26 @@ class CarController extends Controller
         return redirect(route('cars.index'));
     }
 
-    public function validateCar($request){
+    public function validateCar($request)
+    {
 
         return $request->validate([
-            'registration_number'=>'required|digits:8|unique:cars',
-            'manufacturer'=>'required',
-            'currently_available'=>'boolean',
-            'contact_email'=>'required|email',
-            'USD'=>'required|numeric'
+            'registration_number' => 'required|digits:8|unique:cars',
+            'manufacturer' => 'required',
+            'currently_available' => 'boolean',
+            'contact_email' => 'required|email',
+            'USD' => 'required|numeric'
         ]);
     }
 
-    public function validateCarUpdate($request){
+    public function validateCarUpdate($request)
+    {
 
         return $request->validate([
-            'manufacturer'=>'required',
-            'currently_available'=>'boolean',
-            'contact_email'=>'required|email',
-            'USD'=>'required|numeric'
+            'manufacturer' => 'required',
+            'currently_available' => 'boolean',
+            'contact_email' => 'required|email',
+            'USD' => 'required|numeric'
         ]);
     }
 
